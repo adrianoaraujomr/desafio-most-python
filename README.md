eeaecbeafd166a27c4b17e5bb7f9629312491a60
Adriano <adriano@localhost>
2021-09-05 22:02:58 -0300

    adicionado texto no readme, e funcoes que estavam no codigo em c++, reduçoa de ruido e retangulo rotacionado

---
+++ README.md
@@ -0,0 +1,17 @@
+# Desafio MOST
+
+### Objetivo
+
+Fazer uma aplicação que tentar remover ao máximo o espaço em branco ao redor de uma imagem.
+
+### Rodar programa
+
+Para rodar o servidor deve-se ter o python instalado. Com o pip deve-se instalar as bibliotecas presentes no arquivo `requirements.txt` com elas instaladas basta usar o comando:
+
+``` 
+python run.py
+``` 
+
+### Processo
+
+O processo para desenvolvimento da solução foi primeiramente buscar na internet como se poderia resolver o problema em questão. Foi descoberto que uma forma de resolver esse problema é fazer a binarição da imagem e usar das funções prontas do opencv para encontrar o retângulo que contem o conteúdo da imagem. O que foi em suma o que foi feito. O que foi adicionado a mais foi um passo para remoção de noise, consitindo em um algorimto para fazer essa redução e também os algoritmos de erosão seguido por dilatação que também podem remover ruídos.
--- app/utils/image_utils.py
+++ app/utils/image_utils.py
@@ -1,22 +1,38 @@
 import numpy as np
 import cv2
 
-WHITE_COLOR = 255
-GRAY_COLOR_BOUNDRY = 128
+COR_BRANCA = 255
+FORCA_FILTRO = 2
+TAMANHO_KERNEL = 2
+
+def remove_white_borders(imagem):
+    imagem_binaria = gera_imagem_binaria(imagem)
+    imagem_binaria = remocao_ruidos(imagem_binaria)
+    return identifica_retangulo(imagem, imagem_binaria)
+
+def gera_imagem_binaria(imagem):
+    imagem_escala_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    imagem_binaria = cv2.threshold(imagem_escala_cinza, 0, WHITE_COLOR, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    return imagem_binaria
+
+def remocao_ruidos(imagem_base, imagem_binaria):
+    kernel = np.ones((TAMANHO_KERNEL,TAMANHO_KERNEL), np.uint8)
+    imagem_final = cv2.fastNlMeansDenoising(imagem_binaria, h=FORCA_FILTRO)
+    imagem_final = cv2.erode(imagem_final, kernel)
+    imagem_final = cv2.dilate(imagem_final, kernel)
+    return imagem_final
 
 
-def invert_image(image):
-    gray_scale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_scale_image = WHITE_COLOR * \
-        (gray_scale_image < GRAY_COLOR_BOUNDRY).astype(np.uint8)
-    return gray_scale_image
+def identifica_retangulo(imagem, imagem_binaria):
+    coordenadas = cv2.findNonZero(imagem_binaria)
+    x, y, width, height = cv2.boundingRect(coordenadas)
+    return imagem[y:y+height, x:x+width]
 
+def identifica_retangulo_minimo(imagem, imagem_binaria):
+    coordenadas = cv2.findNonZero(imagem_binaria)
+    retangulo = cv2.minAreaRect(coordenadas)
+    matriz_transformacao = cv2.getRotationMatrix2D(retangulo[0], retangulo[2], 1)
+    imagem_rotacionada = cv2.warpAffine(imagem, matriz_transformacao, imagem.shape)
+    imagem_final = cv2.getRectSubPix(imagem_rotacionada, retangulo[1], retangulo[0])
+    return imagem_final
 
-def find_rectangle(gray_scale_image):
-    coordinates = cv2.findNonZero(gray_scale_image)
-    return cv2.boundingRect(coordinates)
-
-
-def remove_white_borders(image):
-    x, y, width, height = find_rectangle(invert_image(image))
-    return image[y:y+height, x:x+width]
